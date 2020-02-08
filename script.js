3c3b1b845d5b02590e587586a12431d76d7a88c0
robin <rogo009git@gmail.com>
2020-02-07 22:56:21 -0800

    update slide function

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 const track = document.querySelector('.carousel-track');
 const slides = Array.from(track.children); // make array of UL children (img slides)
-const nextButton = document.querySelector('.button-left');
-const prevButton = document.querySelector('.button-right');
+const prevButton = document.querySelector('.button-left');
+const nextButton = document.querySelector('.button-right');
 const dotsNav = document.querySelector('.carousel-nav');
 const dots = Array.from(dotsNav.children); // make array out of div w/ dot-buttons
 
 const slideSize = slides[0].getBoundingClientRect(); // shows all dimensions
 const slideWidth = slideSize.width; //show width only
-
 // console.log(slideWidth);
 
 //arrange slides next to one another, must get the slide width and arrange visually
@@ -23,7 +22,16 @@ slides.forEach(setSlidePosition);
 // slides[2].style.left = slideWidth *2 + 'px';
 
 // left arrow moves sldies to left
+
 // right arrow moves slides to right
+
+nextButton.addEventListener('click', e => {
+    const currentSlide = track.querySelector('.current-slide');
+    const nextSlide = currentSlide.nextElementSibling;
+    const amountToMove = nextSlide.style.left;
+    //move to the next slide
+    console.log(amountToMove);
+})
 // nav indicators moves to specific slide
 
 //readME.md 
